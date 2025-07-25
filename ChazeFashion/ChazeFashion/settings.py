9b7594d0d64f3ee57879ba0817bb3e1acfdd99f3
[Mansi Singh] <[mansi273s@gmail.com]>
2025-07-25 21:21:09 +0530

    Prepare project for deployment

--- ChazeFashion/ChazeFashion/settings.py
+++ ChazeFashion/ChazeFashion/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0nlcx_n5$f8p*4bu5c5_@b)89d7wux)m^@o#23m((q09%jrb2l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
