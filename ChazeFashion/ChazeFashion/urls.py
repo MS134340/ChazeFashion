2639ebdf5152a4885a29c23c59d69e1d927449ef
Mansi Singh <115361515+MS134340@users.noreply.github.com>
2025-07-26 00:08:01 +0530

    Update urls.py

--- ChazeFashion/ChazeFashion/urls.py
+++ ChazeFashion/ChazeFashion/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
     path('', include('catalog.urls')),
+
 ]
 
 if settings.DEBUG:
